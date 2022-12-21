5a2cdacb0afc728c6713e920cbc7e0f7cb0fbdd8
Harry Smith <harry@hsmith.net>
2022-12-21 08:31:05 +0000

    Tests for forward moving time and single tracks

--- lumen/tests/running_tracks_with_history_test.rs
+++ lumen/tests/running_tracks_with_history_test.rs
@@ -12,7 +12,10 @@ mod single_track_moving_forward {
 
     use crate::{action, build_environment};
 
-    // Run one action at T1 and generate one history
+    // PLAYHEAD     *
+    //    TRACK ----O----
+    //     TIME     1
+    //  HISTORY     1
     #[test]
     fn one_action_at_t1_generates_one_history() {
         let mut environment = build_environment(1);
@@ -34,9 +37,91 @@ mod single_track_moving_forward {
             Values::make_literal(10.0)
         )
     }
-    // Run two action at T2 and generate one history
-    // Run two action at T1 and two actions at T2 and generate two history
-    // Run one action at T1, then one action at T2, and generate two history
+
+    // PLAYHEAD          *
+    //    TRACK ----O----O----
+    //     TIME     1    2
+    //  HISTORY     1    2
+    #[test]
+    fn two_action_at_t1_generates_one_history() {
+        let mut environment = build_environment(1);
+        let mut track = Track::new();
+        track.add_action(Time::at(0, 0, 1, 0), action(10.0));
+        track.add_action(Time::at(0, 0, 1, 0), action(20.0));
+        environment.add_track(track);
+
+        environment.run_to_time(Time::at(0, 0, 1, 0));
+
+        assert_eq!(environment.history.len(), 1);
+        assert_eq!(
+            environment
+                .fixtures
+                .get(&1)
+                .unwrap()
+                .param(Param::Intensity)
+                .unwrap()
+                .value(),
+            Values::make_literal(20.0)
+        )
+    }
+
+    // PLAYHEAD          *
+    //    TRACK ----O----O----
+    //          ----O----O----
+    //     TIME     1    2
+    //  HISTORY     1    2
+    #[test]
+    fn two_action_at_t1_and_two_action_at_t2_generates_two_history() {
+        let mut environment = build_environment(1);
+        let mut track = Track::new();
+        track.add_action(Time::at(0, 0, 1, 0), action(10.0));
+        track.add_action(Time::at(0, 0, 1, 0), action(20.0));
+        track.add_action(Time::at(0, 0, 2, 0), action(30.0));
+        track.add_action(Time::at(0, 0, 2, 0), action(40.0));
+        environment.add_track(track);
+
+        environment.run_to_time(Time::at(0, 0, 2, 0));
+
+        assert_eq!(environment.history.len(), 2);
+        assert_eq!(
+            environment
+                .fixtures
+                .get(&1)
+                .unwrap()
+                .param(Param::Intensity)
+                .unwrap()
+                .value(),
+            Values::make_literal(40.0)
+        )
+    }
+
+    // PLAYHEAD     >    *
+    //    TRACK ----O----O----
+    //     TIME     1    2
+    //  HISTORY     1    2
+    #[test]
+    fn one_action_at_t1_the_one_action_at_t2_generates_two_history() {
+        let mut environment = build_environment(1);
+        let mut track = Track::new();
+        track.add_action(Time::at(0, 0, 1, 0), action(10.0));
+        track.add_action(Time::at(0, 0, 2, 0), action(20.0));
+        environment.add_track(track);
+
+        environment.run_to_time(Time::at(0, 0, 1, 0));
+        environment.run_to_time(Time::at(0, 0, 2, 0));
+
+        assert_eq!(environment.history.len(), 2);
+        assert_eq!(
+            environment
+                .fixtures
+                .get(&1)
+                .unwrap()
+                .param(Param::Intensity)
+                .unwrap()
+                .value(),
+            Values::make_literal(20.0)
+        )
+    }
 }
 
 // TIME MOVES BACKWARDS - SINGLE TRACK
