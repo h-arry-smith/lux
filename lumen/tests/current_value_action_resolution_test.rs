4693eb0ebdb849d448b33b1de2add4758e3ff8ea
Harry Smith <harry@hsmith.net>
2023-01-22 14:03:26 +0000

    Lumen: Multiple Capturing Fades Test
    Many action applied, with a fade that starts with a CurVal, and
    time fuzzing across them to check state is still as we expect.

--- lumen/tests/current_value_action_resolution_test.rs
+++ lumen/tests/current_value_action_resolution_test.rs
@@ -32,8 +32,6 @@ fn simple() {
 
     environment.run_to_time(Time::at(0, 0, 4, 0), &patch);
 
-    dbg!(&environment.fixtures);
-
     let resolved = environment.fixtures.resolve(Time::at(0, 0, 4, 0), &patch);
 
     assert_eq!(
@@ -46,6 +44,46 @@ fn simple() {
     )
 }
 
+//  0 - 100
+// 50 - 100
+// 75 - 100
+//    *     = 87.50
+#[test]
+fn multi_fade() {
+    let mut environment = Environment::new();
+    environment.fixtures.create_with_id(1);
+    let mut patch = Patch::new();
+    let mut dimmer = FixtureProfile::new();
+    dimmer.set_parameter(Param::Intensity, Parameter::new(0, 0.0, 100.0));
+    patch.patch(1, Address::new(1, 1), &dimmer);
+
+    let mut track = Track::new();
+
+    track.add_action(Time::at(0, 0, 0, 0), fading_action());
+    track.add_action(Time::at(0, 0, 2, 0), capture_fading_action());
+    track.add_action(Time::at(0, 0, 4, 0), capture_fading_action());
+
+    environment.add_track(track);
+
+    // Time Fuzzing
+    environment.run_to_time(Time::at(0, 0, 6, 0), &patch);
+    environment.run_to_time(Time::at(0, 0, 1, 0), &patch);
+    environment.run_to_time(Time::at(0, 0, 3, 0), &patch);
+    environment.run_to_time(Time::at(0, 0, 2, 0), &patch);
+    environment.run_to_time(Time::at(0, 0, 6, 0), &patch);
+
+    let resolved = environment.fixtures.resolve(Time::at(0, 0, 6, 0), &patch);
+
+    assert_eq!(
+        *resolved
+            .get(&1)
+            .unwrap()
+            .get_value(&Param::Intensity)
+            .unwrap(),
+        Values::make_literal(87.50)
+    )
+}
+
 // TODO: This action boilerplate code is only differnetiated by value across
 //       many tests, and should be abstracted to a helper method
 
@@ -54,6 +92,22 @@ fn fading_action() -> Action {
     let query = QueryBuilder::new().all().build();
     let value = Fade::new(
         Box::new(Static::new(Values::make_literal(0.0))),
+        Box::new(Static::new(Values::make_literal(100.0))),
+        Duration::new(4, 0),
+    );
+    let apply = Apply::new(Param::Intensity, Box::new(value));
+    let mut apply_group = ApplyGroup::new(query);
+    apply_group.add_apply(apply);
+    action.add_group(apply_group);
+
+    action
+}
+
+fn capture_fading_action() -> Action {
+    let mut action = Action::new();
+    let query = QueryBuilder::new().all().build();
+    let value = Fade::new(
+        Box::new(CurrentValue::new()),
         Box::new(Static::new(Values::make_literal(100.0))),
         Duration::new(4, 0),
     );
